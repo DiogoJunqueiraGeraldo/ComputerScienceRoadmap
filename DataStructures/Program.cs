8ba96944490d0a425beaf671bc0b120f30da40e8
Diogo <Diogo.Junqueira-ext@ab-inbev.com>
2023-04-15 01:57:56 -0300

    Add: DataStructures.Lists.LinkedList - Reverse, Inserting Operations

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+var linkedList = new DataStructures.Lists.SingleLinkedList<int>();
+
+linkedList.Insert(1);
+linkedList.Insert(2);
+linkedList.Insert(3);
+linkedList.Insert(4);
+linkedList.Insert(5);
+
+
+linkedList.InsertAt(1, 111);
+linkedList.InsertAt(3, 222);
+linkedList.InsertAt(8, 999);
+
+linkedList.Reverse();
+
+Console.WriteLine(linkedList);
